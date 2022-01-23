b033e0097d7f3609be1a489e89e0207ce1df9067
studymate <kumarstudymate@gmail.com>
2022-01-23 14:56:01 +0530

    my react learning

---
+++ react-complete-guide/src/App.js
@@ -0,0 +1,18 @@
+import logo from './logo.svg';
+import './App.css';
+import Person from './Person/Person'
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Hai welcome to reactJS</h1>
+        <Person></Person>
+       
+        
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ react-complete-guide/src/Person/Person.js
@@ -0,0 +1,7 @@
+import React from 'react' ;
+
+const person = () => {
+    return <p> This is a person! and I am {Math.floor(Math.random()*10)} years old </p> 
+};
+
+export default person;
